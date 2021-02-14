b7a4662d833f127283cc40b4b9a8143c83f15560
Filippo Marini <filippo.marini@pd.infn.it>
2021-02-14 21:16:31 +0100

    correctly generate hdl-prj.json

--- json_prova.py
+++ json_prova.py
@@ -1,13 +1,22 @@
 import json
 import os
+import itertools
 '''
     For the given path, get the List of all files in the directory tree
 '''
+def def_ext(x):
+    return {
+        ".vhd": "vhdl",
+        ".v": "verilog",
+        ".sv": "systemverilog"
+    }.get(x)
+
 def getListOfFiles(dirName):
     # create a list of file and sub directories
     # names in the given directory
     listOfFile = os.listdir(dirName)
     allFiles = list()
+    allExt = list()
     # Iterate over all the entries
     for entry in listOfFile:
         # Create full path
@@ -15,19 +24,21 @@ def getListOfFiles(dirName):
         # Retrieve file extension
         filename, file_extension = os.path.splitext(fullPath)
         # If entry is a directory then get the list of files in this directory
-        if file_extension == ".vhd":
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)[0]
+            allExt = allExt + getListOfFiles(fullPath)[1]
+        elif file_extension == ".vhd" or file_extension == ".v" or file_extension == ".sv":
             allFiles.append(fullPath)
-        elif os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-    return allFiles
+            allExt.append(def_ext(file_extension))
+    return allFiles, allExt
 
-def createJson(files):
+def createJson(files, exts):
     data = {}
 
     data['files'] = []
-    for filename in files:
+    for (filename, extname) in zip(files, exts):
         data['files'].append({
-            'language': 'vhdl', 'file': filename
+            'language': extname, 'file': filename
         })
 
     data['options'] = {}
@@ -42,10 +53,17 @@ def main():
     dirName = '.';
 
     # Get the list of all files in directory tree at given path
-    listOfFiles = getListOfFiles(dirName)
+    listOfFiles, listOfExt = getListOfFiles(dirName)
+    #for (filename, fileext) in zip(listOfFiles, listOfExt):
+        #print filename
+        #print fileext
+    #print "****"
     #for filename in listOfFiles:
-    #    print filename
-    hdl_prj = createJson(listOfFiles)
+        #print filename
+    #print "****"
+    #for fileext in listOfExt:
+        #print fileext
+    hdl_prj = createJson(listOfFiles, listOfExt)
 
     #output the json
     with open('hdl-prj.json', 'w') as outfile:
